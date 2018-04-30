b36fabd986c562de794397130b08b9ebe7629337
edwin <esantamar1a77@gmail.com>
2018-04-30 15:39:13 -0700

    update Person.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		person p1= new Person("John",39,12345,"blue");
+		Person p1= new Person("John",39,12345,"blue");
 		
 		System.out.println(p1.getName());
 
--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,57 @@
 public class Person {
 	private String name;
 	private int age;
+	private long id;
+	private String Eyecolor;
+
+	
+	public Person (String name, int age, long id, String Eyecolor) {
+		this.name=name;
+		this.age=age;
+		this.id=id;
+		this.Eyecolor=Eyecolor;
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getEyecolor() {
+		return Eyecolor;
+	}
+
+
+	public void setEyecolor(String eyecolor) {
+		Eyecolor = eyecolor;
+	}
 	
 	
 	}
